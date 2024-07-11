8b04181229a25f2166216bd34f6661555d40ca54
20120454 <dat13102k2@gmail.com>
2024-07-11 16:42:45 +0700

    feat: link to lottery at welcome page

--- src/pages/Lottery/Lottery.tsx
+++ src/pages/Lottery/Lottery.tsx
@@ -1,3 +1,4 @@
+import Meta from '@/components/Meta';
 import { currency } from '@/constants';
 import useNotifications from '@/store/notifications';
 import StarIcon from '@mui/icons-material/Star';
@@ -154,179 +155,183 @@ const Lottery: React.FC = () => {
 	if (!address) return <LoginPrompt />;
 
 	return (
-		<Container
-			maxWidth="lg"
-			sx={{
-				display: 'flex',
-				flexDirection: 'column',
-				minHeight: '100vh',
-			}}
-		>
-			<Box sx={{ flex: 1 }}>
-				<Box
-					sx={{
-						border: '1px solid rgba(34, 197, 94, 0.2)',
-						display: 'flex',
-						justifyContent: 'center',
-						marginBottom: 2,
-						padding: 2,
-					}}
-				>
-					<Typography variant="h6">Last Winner: {lastWinner?.toString()}</Typography>
-					<Typography sx={{ marginLeft: 5 }} variant="h6">
-						Previous winnings:{' '}
-						{lastWinnerAmount && ethers.utils.formatEther(lastWinnerAmount?.toString())}
-					</Typography>
-				</Box>
-
-				{lotteryOperator === address && (
-					<Box display="flex" justifyContent="center">
-						<AdminControls />
+		<>
+			<Meta title="Lottery Game" />
+			<Container
+				maxWidth="lg"
+				sx={{
+					display: 'flex',
+					flexDirection: 'column',
+					minHeight: '100vh',
+				}}
+			>
+				<Box sx={{ flex: 1 }}>
+					<Box
+						sx={{
+							border: '1px solid rgba(34, 197, 94, 0.2)',
+							display: 'flex',
+							justifyContent: 'center',
+							marginBottom: 2,
+							padding: 2,
+						}}
+					>
+						<Typography variant="h6">Last Winner: {lastWinner?.toString()}</Typography>
+						<Typography sx={{ marginLeft: 5 }} variant="h6">
+							Previous winnings:{' '}
+							{lastWinnerAmount && ethers.utils.formatEther(lastWinnerAmount?.toString())}
+						</Typography>
 					</Box>
-				)}
 
-				{winners > 0 && (
-					<Box margin="auto" marginTop={2} maxWidth="md">
-						<Button
-							color="success"
-							fullWidth
-							onClick={onWithdrawWinning}
-							sx={{ animation: 'pulse 2s infinite', fontSize: '1.2rem', padding: 3 }}
-							variant="contained"
-						>
-							Winner Winner Chicken Dinner
-							<Typography variant="h6">
-								Total Winnings: {ethers.utils.formatEther(winners.toString())}
-							</Typography>
-							<Typography variant="body1">Click Here to Withdraw</Typography>
-						</Button>
-					</Box>
-				)}
+					{lotteryOperator === address && (
+						<Box display="flex" justifyContent="center">
+							<AdminControls />
+						</Box>
+					)}
 
-				<Grid
-					alignItems="flex-start"
-					container
-					justifyContent="center"
-					spacing={3}
-					sx={{ marginTop: 2 }}
-				>
-					<Grid item md={6} xs={12}>
-						<Paper elevation={3} sx={{ padding: 2, textAlign: 'center' }}>
-							<Typography variant="h4">The Next Draw</Typography>
-							<Box display="flex" justifyContent="space-between" padding={2}>
-								<Box>
-									<Typography variant="h6">Total Pool</Typography>
-									<Typography variant="h5">
-										{currentWinningReward &&
-											ethers.utils.formatEther(currentWinningReward.toString())}{' '}
-										{currency}
-									</Typography>
-								</Box>
-								<Box>
-									<Typography variant="h6">Tickets Remaining</Typography>
-									<Typography variant="h5">{remainingTickets?.toNumber()}</Typography>
-								</Box>
-							</Box>
-							<CountdownTimer />
-						</Paper>
-					</Grid>
-					<Grid item md={6} xs={12}>
-						<Paper elevation={3} sx={{ padding: 2 }}>
-							<Box alignItems="center" display="flex" justifyContent="space-between">
-								<Typography variant="h6">Price Per Ticket</Typography>
-								<Typography variant="h6">
-									{ticketPrice && ethers.utils.formatEther(ticketPrice?.toString())} {currency}
-								</Typography>
-							</Box>
-							<Box
-								alignItems="center"
-								border={1}
-								borderColor="#004337"
-								display="flex"
-								gap={2}
-								marginY={2}
-								padding={2}
-							>
-								<Typography variant="body1">TICKET</Typography>
-								<TextField
-									InputProps={{
-										disableUnderline: true,
-										endAdornment: <InputAdornment position="end">Qty</InputAdornment>,
-									}}
-									fullWidth
-									inputProps={{ max: 10, min: 1 }}
-									onChange={(e) => setQuantity(Number(e.target.value))}
-									type="number"
-									value={quantity}
-									variant="standard"
-								/>
-							</Box>
-							<Box marginTop={2}>
-								<Box display="flex" justifyContent="space-between">
-									<Typography color="success.main" variant="body1">
-										Total cost tickets
-									</Typography>
-									<Typography color="success.main" variant="body1">
-										{ticketPrice &&
-											Number(ethers.utils.formatEther(ticketPrice?.toString())) * quantity}{' '}
-										{currency}
-									</Typography>
-								</Box>
-								<Box display="flex" justifyContent="space-between">
-									<Typography color="success.main" variant="body2">
-										Service fees
-									</Typography>
-									<Typography color="success.main" variant="body2">
-										{ticketCommission && ethers.utils.formatEther(ticketCommission?.toString())}{' '}
-										{currency}
-									</Typography>
-								</Box>
-								<Box display="flex" justifyContent="space-between">
-									<Typography color="success.main" variant="body2">
-										+ Network Fees
-									</Typography>
-									<Typography color="success.main" variant="body2">
-										TBC
-									</Typography>
-								</Box>
-							</Box>
+					{winners > 0 && (
+						<Box margin="auto" marginTop={2} maxWidth="md">
 							<Button
-								color="primary"
-								disabled={
-									expiration?.toString() < Date.now().toString() ||
-									remainingTickets?.toNumber() === 0
-								}
+								color="success"
 								fullWidth
-								onClick={handleClick}
-								sx={{ marginTop: 2 }}
+								onClick={onWithdrawWinning}
+								sx={{ animation: 'pulse 2s infinite', fontSize: '1.2rem', padding: 3 }}
 								variant="contained"
 							>
-								Buy {quantity} Tickets for{' '}
-								{ticketPrice && Number(ethers.utils.formatEther(ticketPrice.toString())) * quantity}{' '}
-								{currency}
+								Winner Winner Chicken Dinner
+								<Typography variant="h6">
+									Total Winnings: {ethers.utils.formatEther(winners.toString())}
+								</Typography>
+								<Typography variant="body1">Click Here to Withdraw</Typography>
 							</Button>
-						</Paper>
-						{userTicket > 0 && (
-							<Paper elevation={3} sx={{ marginTop: 2, padding: 2 }}>
-								<Typography variant="h6">You have {userTicket} Tickets in this draw</Typography>
-								<Box display="flex" flexWrap="wrap" marginTop={2}>
-									{Array(userTicket)
-										.fill('')
-										.map((_, index) => (
-											<StarIcon color="primary" key={index} />
-										))}
+						</Box>
+					)}
+
+					<Grid
+						alignItems="flex-start"
+						container
+						justifyContent="center"
+						spacing={3}
+						sx={{ marginTop: 2 }}
+					>
+						<Grid item md={6} xs={12}>
+							<Paper elevation={3} sx={{ padding: 2, textAlign: 'center' }}>
+								<Typography variant="h4">The Next Draw</Typography>
+								<Box display="flex" justifyContent="space-between" padding={2}>
+									<Box>
+										<Typography variant="h6">Total Pool</Typography>
+										<Typography variant="h5">
+											{currentWinningReward &&
+												ethers.utils.formatEther(currentWinningReward.toString())}{' '}
+											{currency}
+										</Typography>
+									</Box>
+									<Box>
+										<Typography variant="h6">Tickets Remaining</Typography>
+										<Typography variant="h5">{remainingTickets?.toNumber()}</Typography>
+									</Box>
 								</Box>
+								<CountdownTimer />
 							</Paper>
-						)}
+						</Grid>
+						<Grid item md={6} xs={12}>
+							<Paper elevation={3} sx={{ padding: 2 }}>
+								<Box alignItems="center" display="flex" justifyContent="space-between">
+									<Typography variant="h6">Price Per Ticket</Typography>
+									<Typography variant="h6">
+										{ticketPrice && ethers.utils.formatEther(ticketPrice?.toString())} {currency}
+									</Typography>
+								</Box>
+								<Box
+									alignItems="center"
+									border={1}
+									borderColor="#004337"
+									display="flex"
+									gap={2}
+									marginY={2}
+									padding={2}
+								>
+									<Typography variant="body1">TICKET</Typography>
+									<TextField
+										InputProps={{
+											disableUnderline: true,
+											endAdornment: <InputAdornment position="end">Qty</InputAdornment>,
+										}}
+										fullWidth
+										inputProps={{ max: 10, min: 1 }}
+										onChange={(e) => setQuantity(Number(e.target.value))}
+										type="number"
+										value={quantity}
+										variant="standard"
+									/>
+								</Box>
+								<Box marginTop={2}>
+									<Box display="flex" justifyContent="space-between">
+										<Typography color="success.main" variant="body1">
+											Total cost tickets
+										</Typography>
+										<Typography color="success.main" variant="body1">
+											{ticketPrice &&
+												Number(ethers.utils.formatEther(ticketPrice?.toString())) * quantity}{' '}
+											{currency}
+										</Typography>
+									</Box>
+									<Box display="flex" justifyContent="space-between">
+										<Typography color="success.main" variant="body2">
+											Service fees
+										</Typography>
+										<Typography color="success.main" variant="body2">
+											{ticketCommission && ethers.utils.formatEther(ticketCommission?.toString())}{' '}
+											{currency}
+										</Typography>
+									</Box>
+									<Box display="flex" justifyContent="space-between">
+										<Typography color="success.main" variant="body2">
+											+ Network Fees
+										</Typography>
+										<Typography color="success.main" variant="body2">
+											TBC
+										</Typography>
+									</Box>
+								</Box>
+								<Button
+									color="primary"
+									disabled={
+										expiration?.toString() < Date.now().toString() ||
+										remainingTickets?.toNumber() === 0
+									}
+									fullWidth
+									onClick={handleClick}
+									sx={{ marginTop: 2 }}
+									variant="contained"
+								>
+									Buy {quantity} Tickets for{' '}
+									{ticketPrice &&
+										Number(ethers.utils.formatEther(ticketPrice.toString())) * quantity}{' '}
+									{currency}
+								</Button>
+							</Paper>
+							{userTicket > 0 && (
+								<Paper elevation={3} sx={{ marginTop: 2, padding: 2 }}>
+									<Typography variant="h6">You have {userTicket} Tickets in this draw</Typography>
+									<Box display="flex" flexWrap="wrap" marginTop={2}>
+										{Array(userTicket)
+											.fill('')
+											.map((_, index) => (
+												<StarIcon color="primary" key={index} />
+											))}
+									</Box>
+								</Paper>
+							)}
+						</Grid>
 					</Grid>
-				</Grid>
-				<Box padding={2} textAlign="center">
-					<Typography color="text.secondary" variant="body2">
-						DISCLAIMER: This is not a REAL LOTTERY, it is a demo.
-					</Typography>
+					<Box padding={2} textAlign="center">
+						<Typography color="text.secondary" variant="body2">
+							DISCLAIMER: This is not a REAL LOTTERY, it is a demo.
+						</Typography>
+					</Box>
 				</Box>
-			</Box>
-		</Container>
+			</Container>
+		</>
 	);
 };
 
